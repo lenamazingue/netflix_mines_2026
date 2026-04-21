b82b3a46dd453e826d834da5c42b48f54f52189e
Pauline Normant <pauline.normant@etu.minesparis.psl.eu>
2026-04-21 16:09:10 +0200

    port 8000

--- main.py
+++ main.py
@@ -145,4 +145,4 @@ async def connexion(utilisateur: Utilisateur):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
