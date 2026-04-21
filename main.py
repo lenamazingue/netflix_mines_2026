53f4af417b3a59b5a799d8065336e70f5f403a0a
Lena Mazingue <lenamazingue@gmail.com>
2026-04-21 14:08:08 +0200

    email_non_existant

--- main.py
+++ main.py
@@ -137,9 +137,14 @@ async def create_account(utilisateur: Utilisateur):
 async def connexion(utilisateur: Utilisateur):
     with get_connection() as conn:
         cursor = conn.cursor()
+        #on va tester si l'adresse mail existe bien dans la base
+
         cursor.execute(f"""  SELECT* FROM Utilisateur WHERE AdreseMail='{utilisateur.email}'AND MotDePasse= '{utilisateur.password}') """) 
+
         res = cursor.fetchone()
         adresse_mail= res[0]
+        if adresse_mail is None:
+            raise HTTPException(status_code=401)
         token = jwt.encode({"ad":adresse_mail}, Mot_secret, algorithm = Algorithm)
         return {"access_token": token,
   "token_type": "bearer"}
