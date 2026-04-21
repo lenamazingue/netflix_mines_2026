44a56eb491dec5449b922510bb2b21863dfe1225
Pauline Normant <pauline.normant@etu.minesparis.psl.eu>
2026-04-21 16:52:16 +0200

    without

--- main.py
+++ main.py
@@ -180,10 +180,7 @@ async def create_preferences(authorization: Annotated[str | None, Header()] = No
     }
         
 
-@app.remove("\preferences")
-async def remove_preferences(authorization: Annotated[str | None, Header()] = None):
-    if not authorization:
-        raise HTTPException(status_code=422)
+
     
 
 if __name__ == "__main__":
