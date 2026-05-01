8273850e08af487c564884f2ec660ce9401448d6
Pauline Normant <pauline.normant@etu.minesparis.psl.eu>
2026-05-01 22:02:57 +0200

    pref no authorization

--- main.py
+++ main.py
@@ -218,6 +218,23 @@ async def remove_preferences(genre_id:int,authorization: Annotated[str | None, H
     
     
 
+#@app.get("/genres")
+#async def get_genres():
+#    with get_connection() as conn:
+#        cursor = conn.cursor()
+ #       cursor.execute(f"""SELECT * FROM Genre""" )
+ #       res = cursor.fetchall()
+ #       print(res)
+ #       return res
+
+
+@app.get("/recommendations")
+async def get_recommendations(preferences : int , authorization : Annotated[str | None, Header()] = None):
+    if not authorization:
+        raise HTTPException(status_code = 422)
+    token = authorization.replace("Bearer ", "")
+    
+
 
 if __name__ == "__main__":
     import uvicorn
