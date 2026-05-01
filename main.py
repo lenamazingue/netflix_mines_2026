4fc2c2f2506ab786ccab76b295239e0f0d4e0b86
Pauline Normant <pauline.normant@etu.minesparis.psl.eu>
2026-05-01 23:27:56 +0200

    j'ai changé pref =1

--- main.py
+++ main.py
@@ -256,7 +256,7 @@ async def get_recommendations(authorization : Annotated[str | None, Header()] = 
         cursor.execute(f"""
             SELECT *
             FROM Film
-            WHERE ID_Genre = 1
+            WHERE ID_Genre = {tuple(ids)}
             ORDER BY DateSortie DESC
             LIMIT 5
         """)
